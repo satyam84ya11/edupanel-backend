9c7a94f7e2e0febc11b57168dd370fdb12a9fcef
Satyam chourasiya <satyam84ya11@gmail.com>
2025-12-03 15:03:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(require("cors")())
+
+app.get("/", (req, res) => {
+  res.send("EduPanel Backend Running 🚀")
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log("Server running on " + port))
